8b85223dac81bf3f8e8c9f6af40a2108fcf00e9d
최정윤 <jes2ngyun@gmail.com>
2026-06-13 05:55:03 +0900

    클래스와 객체 실습 추가

---
+++ src/practice/oop/inheritance/abstractexample/Animal.java
@@ -0,0 +1,13 @@
+package practice.oop.inheritance.abstractexample;
+
+// 추상클래스 선언
+abstract class Animal {
+
+    public String name;
+
+    abstract void eat();
+
+    public void sleep() {
+        System.out.println("쿨쿨..");
+    }
+}
---
+++ src/practice/oop/inheritance/abstractexample/Cat.java
@@ -0,0 +1,9 @@
+package practice.oop.inheritance.abstractexample;
+
+public class Cat extends Animal {
+
+    @Override
+    void eat() {
+        System.out.println("냠냥냥!");
+    }
+}
